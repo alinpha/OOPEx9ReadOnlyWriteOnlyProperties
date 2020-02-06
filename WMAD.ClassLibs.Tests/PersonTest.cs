b8c980afa2464b718988d26056d02c7002155a26
Aline Vetrov <alinpha@gmail.com>
2020-02-06 15:22:39 -0400

    update

--- WMAD.ClassLibs.Tests/PersonTest.cs
+++ WMAD.ClassLibs.Tests/PersonTest.cs
@@ -7,8 +7,29 @@ namespace WMAD.ClassLibs.Tests
     public class PersonTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void AgeTest_Positive()
         {
+            Person target = new Person();
+
+            //birth month has passed
+            target.DateOfBirth = DateTime.Now.AddYears(-50).AddMonths(-1);
+            Assert.AreEqual(50, target.Age);
+
+            //birthday is one month away
+            target.DateOfBirth = DateTime.Now.AddYears(-50).AddMonths(1);
+            Assert.AreEqual(49, target.Age);
+
+            //birthday yesterday
+            target.DateOfBirth = DateTime.Now.AddYears(-50).AddDays(-1);
+            Assert.AreEqual(50, target.Age);
+
+            //birthday one day away
+            target.DateOfBirth = DateTime.Now.AddYears(-50).AddDays(1);
+            Assert.AreEqual(49, target.Age);
+
+            //birthday is today
+            target.DateOfBirth = DateTime.Now.AddYears(-50);
+            Assert.AreEqual(50, target.Age);
         }
     }
 }
